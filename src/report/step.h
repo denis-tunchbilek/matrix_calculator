6bad5fcfa8cc3317480499acd9c187ab154323d7
ArtemKostrichko <csc0kiee@gmail.com>
2026-03-12 23:39:52 +0300

    add abstract Step base

---
+++ src/report/step.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+#include "core/matrix.h"
+#include "util/value.h"
+
+namespace report {
+
+    class Step {
+    public:
+        virtual ~Step() = default;
+
+        [[nodiscard]] virtual std::string name() const = 0;
+        [[nodiscard]] virtual util::Value run(const core::Matrix& matrix) = 0;
+    };
+
+} // namespace report
