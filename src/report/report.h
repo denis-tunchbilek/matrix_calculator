690fc978961bdd0ddb5e511e57fb9991e622e940
ArtemKostrichko <csc0kiee@gmail.com>
2026-03-12 23:41:23 +0300

    add StepRunner skeleton with unique_ptr step storage

---
+++ src/report/report.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <memory>
+#include <ostream>
+#include <vector>
+
+#include "core/matrix.h"
+#include "report/step.h"
+
+namespace report {
+
+    class StepRunner {
+    public:
+        void addStep(std::unique_ptr<Step> step);
+
+        void run(const core::Matrix& matrix, std::ostream& output) const;
+
+    private:
+        std::vector<std::unique_ptr<Step>> steps_;
+    };
+
+} // namespace report
