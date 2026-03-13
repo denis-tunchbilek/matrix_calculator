f4bd56d2d1ffa3794c75bb2d21ea3b6d4a7b13c0
denis-tunchbilek <86554612+denis-tunchbilek@users.noreply.github.com>
2026-03-13 10:48:11 +0300

    test_print

---
+++ tests/test_print.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <sstream>
+
+#include "io/matrix_printer.h"
+#include "util/value_printer.h"
+
+TEST(MatrixPrinterTest, NormalizesAlmostZeroToZero) {
+    matrix::core::Matrix matrix(1, 2);
+    matrix.at(0, 0) = 1e-12;
+    matrix.at(0, 1) = 2.5;
+
+    matrix::io::MatrixPrinter printer;
+    std::ostringstream out;
+    printer.print(out, matrix);
+
+    EXPECT_NE(out.str().find("0.0000"), std::string::npos);
+    EXPECT_NE(out.str().find("2.5000"), std::string::npos);
+}
+
+TEST(ValuePrinterTest, PrintsVariantUsingVisit) {
+    auto printer = std::make_shared<const matrix::io::MatrixPrinter>();
+    std::ostringstream out;
+
+    matrix::util::printValue(out, matrix::util::Value(std::string("hello")), printer);
+
+    EXPECT_EQ(out.str(), "hello");
+}
